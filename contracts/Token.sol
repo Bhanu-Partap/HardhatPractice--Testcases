5f3cfdd07bc63f2fcbc02042a7c6bf44fe8d9c65
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-06-13 12:20:30 +0530

    HardhatPractice-Testcases

---
+++ contracts/Token.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity  ^0.8.0;
+
+contract Token{
+    string public name = "Hardhat things";
+    string public symbol= "HTX";
+    uint public totalsupply = 10000;
+    
+    address public owner;
+    mapping(address => uint) balances;
+
+    constructor(){
+        balances[msg.sender] = totalsupply;
+        owner = msg.sender;
+    }
+
+    function transfer(address to, uint amount) external {
+        require(balances[msg.sender]>= amount,"Not enought Tokens");
+        balances[msg.sender] -= amount;
+        balances[to]+= amount;
+    }
+
+    function balanceOf(address account) external view returns(uint256){
+        return balances[account];
+    }
+    function total_Supply() external view returns(uint256){
+        return balances[msg.sender];
+    }
+
+}
+
---
+++ test/Lock.js
@@ -0,0 +1,47 @@
+ //================Token Contract ==================//
+
+const { expect } = require("chai");
+
+describe("Token contract", () => {
+  let Token, token, owner, addr1, addr2;
+
+  beforeEach(async () => {
+    token = await ethers.getContractFactory("Token");
+    Token = await token.deploy();
+    [owner, addr1, addr2, _] = await ethers.getSigners();
+  });
+
+  describe("Bhanu", () => {
+    it("Should set the right owner", async () => {
+      expect(await Token.owner()).to.equal(owner.address);
+    });
+    it("Should assign the total supply of tokens to the owner", async () => {
+      const ownerBalance = await Token.balanceOf(owner.address);
+      expect(await Token.total_Supply()).to.equal(ownerBalance);
+    });
+  });
+
+  // descibe("Transcations", () => {
+  //   it("Should transfer tokens between accounts", async () => {
+  //     await token.transfer(addr1.address, 50);
+  //     const addr1Balance = await token.balanceOf(addr1.address);
+  //     expect(addr1Balance).to.equal(50);
+  //   });
+
+  //   it("Should fail if sender doesnt have enought tokens", async () => {
+  //     const initialOwnerBalanace = await token.balanceOf(owner.address);
+
+  //     await expect(
+  //       token.connect(addr1).transfer(owner.address, 1)
+  //     ).to.be.revertedWith("Not enough tokens");
+
+  //     expect(await token.balanceOf(owner.address)).to.equal(
+  //       initialOwnerBalanace
+  //     );
+  //   });
+  //   it('Should update the balance after transfers', async()=>{
+  //     const initialOwnerBalanace = await token.balanceOf(owner.address);
+      
+  //   })
+  // });
+});
