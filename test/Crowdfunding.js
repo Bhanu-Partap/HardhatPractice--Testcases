8c6182d0b583a650caf0e2e5f6f08790328a2ef6
Bhanu-Partap <bhanupartap25275@gmail.com>
2023-06-15 10:43:43 +0530

    testcase practice

--- test/Crowdfunding.js
+++ test/Crowdfunding.js
@@ -1,9 +1,14 @@
-const {expect} = require("chai")
+const {expect} = require("chai");
 
 describe("Crowdfunding Contract", ()=>{
     beforeEach(async()=>{
         crowdfunding = await ethers.getContractFactory("Crowdfunding");
         Crowdfunding = await crowdfunding.deploy();
-        []
+        [owner, addr1, addr2] = await ethers.getSigners();
+    });
+    describe("Test case", ()=>{
+        it('should it returning the right value', async()=>{
+            await Crowdfunding.userProjects.call(1);
+        })
     })
 })
